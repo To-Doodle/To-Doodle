c9d56a5ca97d60954e6a8eba1bb291f6fe993d5d
Dimitri Walters <me@dimi.io>
2022-06-19 23:02:52 -0400

    Add class for Room Database access point

---
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/data/AppDatabase.kt
@@ -0,0 +1,54 @@
+package ca.uwaterloo.cs.todoodle.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import java.io.File
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+
+    companion object {
+
+        private const val TAG = "AppDatabase"
+
+        const val VERSION = 1
+        private const val DATABASE_NAME = "todoodle.db"
+
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        /**
+         * Gets and returns the database instance if exists; otherwise, builds a new database.
+         * @param context The context to access the application context.
+         * @return The database instance.
+         */
+        fun getInstance(context: Context): AppDatabase =
+            instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+
+        /**
+         * Builds and returns the database.
+         * @param appContext The application context to reference.
+         * @return The built database.
+         */
+        private fun buildDatabase(appContext: Context): AppDatabase {
+            val filesDir = appContext.getExternalFilesDir(null)
+            val dataDir = File(filesDir, "data")
+            if (!dataDir.exists())
+                dataDir.mkdir()
+
+            val builder =
+                Room.databaseBuilder(
+                    appContext,
+                    AppDatabase::class.java,
+                    DATABASE_NAME
+                ).fallbackToDestructiveMigration()
+
+            return builder.build()
+        }
+    }
+}
