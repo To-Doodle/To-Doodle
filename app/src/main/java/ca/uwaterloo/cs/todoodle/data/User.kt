cdebd53de95a38c4858d3e090f018f6efe3b60a9
Dimitri Walters <me@dimi.io>
2022-06-19 22:57:43 -0400

    Add data entity and dao for User model

---
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/data/User.kt
@@ -0,0 +1,12 @@
+package ca.uwaterloo.cs.todoodle.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+)
