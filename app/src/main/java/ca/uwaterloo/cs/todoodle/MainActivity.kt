6d395789c656a294f54f90a480a8c0cce1ef6dbc
Dimitri Walters <me@dimi.io>
2022-07-10 15:07:40 -0400

    Add navigation menu item for todo list fragment

--- app/src/main/java/ca/uwaterloo/cs/todoodle/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_todo
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
