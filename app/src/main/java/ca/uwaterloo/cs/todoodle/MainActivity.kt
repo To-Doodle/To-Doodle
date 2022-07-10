60f91d17937ef87842fcd460181b45b27ee03719
Dimitri Walters <me@dimi.io>
2022-07-10 18:38:07 -0400

    Remove unused menu items

--- app/src/main/java/ca/uwaterloo/cs/todoodle/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_doodle, R.id.nav_slideshow, R.id.nav_todo
+                R.id.nav_todo, R.id.nav_doodle
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
