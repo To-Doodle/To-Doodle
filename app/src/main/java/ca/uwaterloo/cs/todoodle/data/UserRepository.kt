775fbe958c905c35d0574f57a508f8464c039c6c
Siyuan Zuo <53063815+legenddaniel@users.noreply.github.com>
2022-07-17 00:06:11 -0400

    Add completed achievements update query

--- app/src/main/java/ca/uwaterloo/cs/todoodle/data/UserRepository.kt
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/data/UserRepository.kt
@@ -4,6 +4,16 @@ class UserRepository(private val userDao: UserDao) {
 
     /**
      * Get user by id
+     * @param userId User id
+     * @return User object
      */
     fun findById(userId: Int): User = userDao.findById(userId)
+
+    /**
+     * Update the completed achievements after each DB manipulation
+     * @param userId User id
+     * @param achievements The new completed achievements
+     */
+    fun updateCompletedAchievements(userId: Int, achievements: HashMap<String, Int>) =
+        userDao.updateCompletedAchievements(userId, achievements)
 }
