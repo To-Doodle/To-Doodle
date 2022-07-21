bd7914efbace9f3ce576cfe90f3fc71acdf785cc
Rohan Afsan <notrohanaf@gmail.com>
2022-07-21 15:52:38 -0400

    Task Dialog changes. Not completely working yet

--- app/src/main/java/ca/uwaterloo/cs/todoodle/RecycleViewAdapter.kt
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/RecycleViewAdapter.kt
@@ -5,9 +5,10 @@ import androidx.recyclerview.widget.RecyclerView
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import ca.uwaterloo.cs.todoodle.data.AppDatabase
 import ca.uwaterloo.cs.todoodle.data.Task
 import ca.uwaterloo.cs.todoodle.data.TaskDao
@@ -18,24 +19,30 @@ class RecycleViewAdapter(private val titles: List<String>, private val deadlines
     private lateinit var dialog: TaskDialogFragment
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         val itemTitle: TextView = itemView.findViewById(R.id.title_view)
         val itemDue: TextView = itemView.findViewById(R.id.date_view)
+        val taskDialogTitle: TextView = itemView.findViewById(R.id.taskdialog_title)
         var itemTask: Task = Task(0, "", "")
-
         init {
             itemView.setOnClickListener { v: View ->
-                Toast.makeText(itemView.context, "Task complete!", Toast.LENGTH_SHORT).show()
-                itemTitle.text = "DONE!"
-                dao = AppDatabase.getInstance(itemView.context).taskDao()
+
                 dialog = TaskDialogFragment()
-//                dialog.show('','TaskDialog')
+                val fragmentManager: FragmentManager = (itemView.context as FragmentActivity).supportFragmentManager
+                dialog.show(fragmentManager, "tag")
+
+                dao = AppDatabase.getInstance(itemView.context).taskDao()
                 dao.delete(itemTask)
+                // I want to show the dialog fragment when I click here
+//                dialogFragment = TaskDialogFragment()
+//                dialogFragment.show(childFragmentManager, "taskDialog")
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
+
         return ViewHolder(v)
     }
 
@@ -47,6 +54,7 @@ class RecycleViewAdapter(private val titles: List<String>, private val deadlines
     // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.itemTitle.text = titles[position]
+        holder.taskDialogTitle.text = titles[position]
         holder.itemDue.text = deadlines[position]
         holder.itemTask = tasks[position]
     }
