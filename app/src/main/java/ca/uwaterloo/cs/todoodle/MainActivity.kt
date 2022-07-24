7ff74bd79c8b66c3bb5f3eade2f4680c3d73da4a
Siyuan Zuo <53063815+legenddaniel@users.noreply.github.com>
2022-07-24 18:07:38 -0400

    Add achievement dialog

--- app/src/main/java/ca/uwaterloo/cs/todoodle/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import ca.uwaterloo.cs.todoodle.data.AchievementRepository
 import ca.uwaterloo.cs.todoodle.databinding.ActivityMainBinding
+import ca.uwaterloo.cs.todoodle.ui.achievements.AchievementDialogFragment
 import ca.uwaterloo.cs.todoodle.ui.achievements.AchievementsFragment
+import ca.uwaterloo.cs.todoodle.ui.rewards.RewardDialogFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,9 +43,11 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        // Use for testing achievement dialog
 //        binding.appBarMain.coinIndicator2.setOnClickListener{
-//            val intent = Intent(this, AchievementsFragment::class.java)
-//            startActivity(intent)
+//            val dialog = AchievementDialogFragment("series_task_1")
+//            val manager = supportFragmentManager
+//            dialog.show(manager, "achievement")
 //        }
 
         // Display user achievement points
--- app/src/main/java/ca/uwaterloo/cs/todoodle/ui/pending/PendingFragment.kt
+++ app/src/main/java/ca/uwaterloo/cs/todoodle/ui/pending/PendingFragment.kt
@@ -86,7 +86,7 @@ class PendingFragment : Fragment() {
                 categoryList,
                 notesList,
                 keysList,
-                achievementRepository
+                achievementRepository,
             )
         }
 
